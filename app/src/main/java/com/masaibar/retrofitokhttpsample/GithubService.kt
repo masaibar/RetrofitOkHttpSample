592f137245cadf2121dad9dbdd2d9d06d5af5ee0
masaibar <masaibar.dev@gmail.com>
2017-10-30 23:06:58 +0900

    あと一歩な気がする

--- app/src/main/java/com/masaibar/retrofitokhttpsample/GithubService.kt
+++ app/src/main/java/com/masaibar/retrofitokhttpsample/GithubService.kt
@@ -1,10 +1,12 @@
 package com.masaibar.retrofitokhttpsample
 
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GithubService {
     @GET("/users/{user}")
-    fun getUserInfo(@Path("user") user: String): Call<User>
+    fun getUserInfoAsObservable(@Path("user") user: String): Observable<User>
+    fun getUserInfoAsCall(@Path("user") user: String): Call<User>
 }
--- app/src/main/java/com/masaibar/retrofitokhttpsample/MainActivity.kt
+++ app/src/main/java/com/masaibar/retrofitokhttpsample/MainActivity.kt
@@ -4,14 +4,23 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import org.reactivestreams.Subscriber
 
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val BASE_URL = "https://api.github.com/"
+        const val USER = "masaibar"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +30,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun get() {
+        getAsObservable()
+        getAsCall()
+    }
+
+    //http://sssslide.com/speakerdeck.com/shige0501/25-rxjava2-plus-okhttp-plus-retrofitru-men あと一歩？                .
+    fun getAsObservable() {
         val retrofit = Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
+                .baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
         val service = retrofit.create<GithubService>(GithubService::class.java)
+        val userObservable = service.getUserInfoAsObservable(USER)
+        userObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe()
+    }
 
-        val userCall = service.getUserInfo("masaibar")
+    fun getAsCall() {
+        val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        val service = retrofit.create<GithubService>(GithubService::class.java)
+        val userCall = service.getUserInfoAsCall(USER)
 
         userCall.enqueue(object : Callback<User> {
             override fun onResponse(call: Call<User>, response: Response<User>) {
